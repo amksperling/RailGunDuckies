2959f9c1f4b4cd0d6dde97729982bd7b37839ece
Adam Sperling <amksperling@gmail.com>
2012-09-24 22:18:37 -0500

    Basic Lighting Working

--- Rail Gun Duckies/Rail Gun Duckies/Duckie.cpp
+++ Rail Gun Duckies/Rail Gun Duckies/Duckie.cpp
@@ -22,77 +22,97 @@ Duckie::~Duckie() {
 void Duckie::render() {
 
 	GLUquadric *q = gluNewQuadric();
-	
-	
-	// draw the body
-	glPushMatrix();
-	glScaled(1, .7, 1.2);
-	glColor3d(1, 1, 0);
-	gluSphere(q,1,100,100);
-	glPopMatrix();
 
-	// draw the head
-	glPushMatrix();
-	glScaled(.4, .4, .4);
-	glTranslated(0, 1.8, 2.2);
-	gluSphere(q,1,100,100);
-	glPopMatrix();
+	//set up material parameters
+	//numbers from http://devernay.free.fr/cours/opengl/materials.html
+	//
+	//yellow plastic
+	GLfloat material_ambient[] = {.00, .00, 0, 1};
+	GLfloat material_diffuse[] = { .5, .5, 0, 1 };
+    GLfloat material_specular[] = { .6, .6, .5, 1 };
+    GLfloat material_shininess[] = { .25 * 128 };
 
-	// draw the wings
-	glPushMatrix();
-	glScaled(.3, .4, .75);
-	glColor3d(1, 1, 0);
-	glTranslated(2.9, 0, 0);
-	gluSphere(q, 1, 100, 100);
-	glPopMatrix();
+	//yellow rubber
+	//GLfloat material_ambient[] = {.05, .05, 0, 1};
+	//GLfloat material_diffuse[] = { .5, .5, .4, 1 };
+	//GLfloat material_specular[] = { .7, .7, .04, 1 };
+	//GLfloat material_shininess[] = { .078125 * 128 };
+	glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, material_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, material_shininess);
 
-	glPushMatrix();
-	glScaled(.3, .4, .75);
-	glColor3d(1, 1, 0);
-	glTranslated(-2.9, 0, 0);
-	gluSphere(q, 1, 100, 100);
-	glPopMatrix();
+	if (q) {
+		// draw the body
+
+		glPushMatrix();
+		glScaled(1, .7, 1.2);
+		glColor3d(1, 1, 0);
+		gluSphere(q,1,100,100);
+		glPopMatrix();
+
+		// draw the head
+		glPushMatrix();
+		glScaled(.4, .4, .4);
+		glTranslated(0, 1.8, 2.2);
+		gluSphere(q,1,100,100);
+		glPopMatrix();
+
+		// draw the wings
+		glPushMatrix();
+		glScaled(.3, .4, .75);
+		glColor3d(1, 1, 0);
+		glTranslated(2.9, 0, 0);
+		gluSphere(q, 1, 100, 100);
+		glPopMatrix();
+
+		glPushMatrix();
+		glScaled(.3, .4, .75);
+		glColor3d(1, 1, 0);
+		glTranslated(-2.9, 0, 0);
+		gluSphere(q, 1, 100, 100);
+		glPopMatrix();
 
 
-	// draw the eyes
-	glPushMatrix();
-	glScaled(.1, .1, .1);
-	glColor3d(1, 1, 1);
-	glTranslated(2, 10, 11.3);
-	gluSphere(q, 1, 100, 100);
-	glPopMatrix();
+		// draw the eyes
+		glPushMatrix();
+		glScaled(.1, .1, .1);
+		glColor3d(1, 1, 1);
+		glTranslated(2, 10, 11.3);
+		gluSphere(q, 1, 100, 100);
+		glPopMatrix();
 
-	glPushMatrix();
-	glScaled(.1, .1, .1);
-	glColor3d(1, 1, 1);
-	glTranslated(-2, 10, 11.3);
-	gluSphere(q, 1, 100, 100);
-	glPopMatrix();
+		glPushMatrix();
+		glScaled(.1, .1, .1);
+		glColor3d(1, 1, 1);
+		glTranslated(-2, 10, 11.3);
+		gluSphere(q, 1, 100, 100);
+		glPopMatrix();
 
-	//pupils
-	glPushMatrix();
-	glScaled(.05, .05, .05);
-	glColor3d(0, 0, 1);
-	glTranslated(4, 20, 24);
-	gluSphere(q, 1, 100, 100);
-	glPopMatrix();
+		//pupils
+		glPushMatrix();
+		glScaled(.05, .05, .05);
+		glColor3d(0, 0, 1);
+		glTranslated(4, 20, 24);
+		gluSphere(q, 1, 100, 100);
+		glPopMatrix();
 
-	glPushMatrix();
-	glScaled(.05, .05, .05);
-	glColor3d(0, 0, 1);
-	glTranslated(-4, 20, 24);
-	gluSphere(q, 1, 100, 100);
-	glPopMatrix();
+		glPushMatrix();
+		glScaled(.05, .05, .05);
+		glColor3d(0, 0, 1);
+		glTranslated(-4, 20, 24);
+		gluSphere(q, 1, 100, 100);
+		glPopMatrix();
 
-	// draw the beak
-	glPushMatrix();
-	glScaled(.25, .1, .5);
-	glColor3d(1, .5, 0);
-	glTranslated(0, 7.5, 2.35);
-	gluCylinder(q, 1, 0, 1, 100, 100);
-	glPopMatrix();
+		// draw the beak
+		glPushMatrix();
+		glScaled(.25, .1, .5);
+		glColor3d(1, .5, 0);
+		glTranslated(0, 7.5, 2.35);
+		gluCylinder(q, 1, 0, 1, 100, 100);
+		glPopMatrix();
 
-	
+	}
 
 	// draw lines separating beak
 	//glColor3d(0, 0, 0);
--- Rail Gun Duckies/Rail Gun Duckies/DuckieDriver.cpp
+++ Rail Gun Duckies/Rail Gun Duckies/DuckieDriver.cpp
@@ -16,82 +16,36 @@ double aspect = 1;
 bool isWireFrame = false;
 bool paused = false;
 bool isFullScreen = false;
+bool duckFullSize = false;
 
 double period = 1000/60;
 
 double pScale = .01;
 
-//void DrawDuckie() {
-//GLUquadric *q = gluNewQuadric();
-//	
-//	
-//	// draw the body
-//	glPushMatrix();
-//	glScaled(1, .7, 1.2);
-//	glColor3d(1, 1, 0);
-//	gluSphere(q,1,100,100);
-//	glPopMatrix();
-//
-//	// draw the head
-//	glPushMatrix();
-//	glScaled(.4, .4, .4);
-//	glTranslated(0, 1.8, 2.2);
-//	gluSphere(q,1,100,100);
-//	glPopMatrix();
-//
-//	// draw the wings
-//	glPushMatrix();
-//	glScaled(.3, .4, .75);
-//	glColor3d(1, 0, 0);
-//	glTranslated(3, 0, 0);
-//	gluSphere(q, 1, 100, 100);
-//	glPopMatrix();
-//
-//	glPushMatrix();
-//	glScaled(.3, .4, .75);
-//	glColor3d(1, 0, 0);
-//	glTranslated(-3, 0, 0);
-//	gluSphere(q, 1, 100, 100);
-//	glPopMatrix();
-//
-//
-//	// draw the eyes
-//	glPushMatrix();
-//	glScaled(.1, .1, .1);
-//	glColor3d(1, 1, 1);
-//	glTranslated(2, 10, 11.3);
-//	gluSphere(q, 1, 100, 100);
-//	glPopMatrix();
-//
-//	glPushMatrix();
-//	glScaled(.1, .1, .1);
-//	glColor3d(1, 1, 1);
-//	glTranslated(-2, 10, 11.3);
-//	gluSphere(q, 1, 100, 100);
-//	glPopMatrix();
-//
-//
-//	// draw the beak
-//	glPushMatrix();
-//	glScaled(.25, .1, .5);
-//	glColor3d(1, .5, 0);
-//	glTranslated(0, 7.5, 2.5);
-//	gluCylinder(q, 1, 0, 1, 100, 100);
-//
-//	// draw lines separating beak
-//	//glColor3d(0, 0, 0);
-//	//glBegin(GL_LINE);
-//	//glVertex3d(2, 7.5, .5);
-//	//glVertex3d(3, 7.5, 1);
-//	//glEnd();
-//	//glPopMatrix();
-//
-//	gluDeleteQuadric(q);
-//}
-void Sphere() {
-	
-}
 
+
+//InitGL function to handle all GL initializations
+// for the main function. Handles lighting and depth so far...
+void initGL() {
+
+	glClearColor (0.0, 0.0, 0.0, 0.0);
+	glShadeModel (GL_SMOOTH);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_COLOR_MATERIAL);
+	glEnable(GL_NORMALIZE);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glLightModelf(GL_LIGHT_MODEL_TWO_SIDE , 0.0);
+	glLightModelf(GL_LIGHT_MODEL_LOCAL_VIEWER , 1.0);
+
+	//set up light position
+	GLfloat light_position[] = { 2 , 2, 1, 0 };
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+
+}
 void DuckieDisplayFunc() {
 	// use a switch to toggle between modes
 	// each mode has a separate function
@@ -134,12 +88,15 @@ void DuckieDisplayFunc() {
 	glTranslated(3, 0, 0);
 	glRotated(elapsed_time * 60, 1, 0, 0);
 
-	if (pScale < 1) {
+	// grow the duck
+	if (pScale <= 1) {
 		glScaled(pScale, pScale, pScale);
+		pScale += .001;
 	}
+
 	p->render();
 
-	pScale += .001;
+
 	
 	delete p;
 	glPopMatrix();
@@ -206,10 +163,9 @@ int main(int argc, char *argv[]) {
 	glutInitWindowSize(init_width, init_height);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 	glutCreateWindow("Duckie Driver");
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_COLOR_MATERIAL);
-	glEnable(GL_DEPTH_TEST);
+	
+	initGL();
+
 	glutDisplayFunc(DuckieDisplayFunc);
 	glutReshapeFunc(ReshapeFunc); // what function called if resized window?
 	glutKeyboardFunc(KeyboardFunc); // what function called if keypressed?
