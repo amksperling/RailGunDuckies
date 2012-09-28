006c99a663bcdb95e623704ff6484db4cbeef953
Jacob Hanshaw <guitarboy667@aim.com>
2012-09-28 15:18:42 -0500

    Silvered. Will edit more later

--- Rail Gun Duckies/Rail Gun Duckies/RailGun.cpp
+++ Rail Gun Duckies/Rail Gun Duckies/RailGun.cpp
@@ -11,11 +11,22 @@ RailGun::~RailGun() {
 }
 
 void RailGun::drawRailGun() {
-	Prism *endRailGun = new Prism(1.0f,1.0f,0.5f,0.0f,0.0f,1.0f);
+	//reflective table
+	GLfloat material_ambient[] = {.19225, .19225, 0.19225, 1};
+	GLfloat material_diffuse[] = { .50754, .50754, 0.50754, 1 };
+    GLfloat material_specular[] = { .502873, .502873, .508273, 1 };
+    GLfloat material_shininess[] = { .4 * 128 };
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, material_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, material_shininess);
+
+	Prism *endRailGun = new Prism(1.0f,1.0f,0.5f,0.511f,0.535f,0.586f);
 	endRailGun->drawPrism();
 	glTranslated(0, 0, -5.5); 
 	endRailGun->drawPrism();
-	Prism *connectorStrutRailGun = new Prism(0.25f,0.25f,5.0f,0.0f,1.0f,0.0f);
+	Prism *connectorStrutRailGun = new Prism(0.25f,0.25f,5.0f,0.511f,0.535f,0.586f);
 	glTranslated(-0.375f, 0.375f, 2.75f);
 	connectorStrutRailGun->drawPrism();
 	glTranslated(0.75f, 0.0f, 0.0f);
