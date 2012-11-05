16409579111849d9cdf0029672c0e887af974817
Adam Sperling <amksperling@gmail.com>
2012-11-05 17:46:56 -0600

    scene.h commented and cleaned

--- Rail Gun Duckies/Rail Gun Duckies/Scene.h
+++ Rail Gun Duckies/Rail Gun Duckies/Scene.h
@@ -1,3 +1,10 @@
+/*
+	The Scene class handles all aspects of the application.
+	It uses every other object to draw the necessary scene and play the game.
+	It also handles all game mechanics and automation.
+
+*/
+
 #pragma once
 #include "Duckie.h"
 #include "RailGun.h"
@@ -18,6 +25,8 @@
 using namespace std;
 using namespace glm;
 
+//Enum used for the 3 difficulty modes
+//default is normal
 enum DIFFICULTY {
 	EASY,
 	NORMAL,
@@ -36,19 +45,29 @@ public:
 	void runBeautyMode(int beautyMode);
 	void runGameMode(bool runForever, double timeStep, Window & w);
 
+
+	//fire the duck
 	void fire();
+
+	//reset the game
+	void resetGame();
+
+	//cycle through the 3 difficulties
+	void cycleDifficulty();
+
+
+	//move the rail gun based on mouse movement and position in the window
+	//this function converts pixels to degrees
 	void moveRailGun(int x, int y, const Window & w);
+
+	//increase/decrease gun power (0-100)
 	void increaseGunPower(double higher);
 	void decreaseGunPower(double lower);
+
+	//collision detection
 	void checkForCollisions (Window & w); //(Object movingItem, Object otherObjects[], Window & w);
 
-//	void duckBeauty();
-//	void railGunBeauty();
-//	void balloonBeauty();
-//	void playGame();
-//	void playGameForever();
-
-
+	//inline getters for all instance variables
 	inline int getScore() const { return this->score; }
 	inline int getDifficulty() const { return this->difficulty; }
 	inline int getDucksRemaining() const { return this->ducksRemaining; }
@@ -59,29 +78,39 @@ public:
 	inline string getDifficultyString() const { return this->difficulty_string; }
 	inline vector<Balloon> & getBalloons() { return this->balloons; }
 	
-	void resetGame();
-	void cycleDifficulty();
-
+	
 private:
 
 	vector<Balloon> balloons; // list of all balloons
-	Balloon aBalloon;
-	RailGun theGun;
+	Balloon aBalloon; //used for beauty mode
+
+	//we only use one gun and one duck the whole time!
+	//save memory!
+	RailGun theGun; 
 	Duckie theDuck;
+
+	//World is used for beauty modes, skybox is for the game
 	World theWorld;
 	SkyBox skyBox;
 
 	static bool gameWon;
 	static bool gameOver;
-	bool balloonHit;
+	static bool balloonHit;
 
 	//used to draw the world in which our objects are placed
 	void renderWorld();
+
+	//displays point text above balloons
 	void displayBalloonPointValue(Balloon & b);
-//	void placeGun();
-//	void placeDuck();
+
+	//generate random positions for balloons and place them in the world
 	void placeBalloons();
+
+	//put the duck back on the gun
 	void resetDuck();
+
+	//automates gun movement
+	// see function for implemnetation details
 	void automateGun();
 
 	static int score;
